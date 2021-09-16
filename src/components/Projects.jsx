4e6cc5cebfb8b5673843d2199d3ddf9221a8311e
Smaragda Kalogeropoulou <smaragda_kal@hotmail.com>
2021-09-16 22:49:46 +0200

    major changes - projects component adjustmentnts and global css variables added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,53 +1,89 @@
 import React from 'react';
 import './AboutMe.scss';
 import './Projects.scss';
+import vooraad from './../picture/voorrad.png';
+import marketplace from './../picture/marketplace.png';
+import foodgame from './../picture/foodgame.png';
 
 const Projects = React.forwardRef((props, ref) => {
   return (
     <section className="projects" ref={ref}>
       <div className="wrapper">
-        <h3>
-          <a
-            href="https://foodgame.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            The Food Game
-          </a>
-        </h3>
-        <p>sDescription - inspired by my vegan lifestyle</p>
+        <a
+          href="https://foodgame.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <h3 className="project_title">The Food Game</h3>
+          {/* <p>A fun 80's style game, inspired by my vegan lifestyle.</p> */}
+          <img
+            className=" project-img foodgame"
+            src={foodgame}
+            alt="website look"
+          />
+        </a>
+        <a
+          className="project_button"
+          href="https://github.com/smara-gda/project-1-food-game"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
       </div>
 
       <div className="wrapper">
-        <h3>
-          <a
-            href="https://voorrad.herokuapp.com/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Vorraad
-          </a>
-        </h3>
-        <p>
-          Pair programmed with one other classmate - inspired by my classmate’s
-          google sheets for keeping an eye on his finances.
-        </p>
+        <a
+          href="https://voorrad.herokuapp.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <h3 className="project_title">Vorraad</h3>
+          {/* <p>
+            Pair programmed with one other classmate - inspired by my
+            classmate’s google sheets for keeping an eye on his finances.
+          </p> */}
+          <img
+            className=" project-img vooraad"
+            src={vooraad}
+            alt="website look"
+          />
+        </a>
+        <a
+          className="project_button"
+          href="https://github.com/victornast/voorrad"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
       </div>
 
-      <div className="wrapper">
-        <h3>
-          <a
-            href="https://getstuffdone.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            MarketPlace
-          </a>
-        </h3>
-        <p>
-          Pair programmed with classmates - inspired by my classmate who wanted
-          to bring people closer and allow them to help each other
-        </p>
+      <div className="wrapper marketplace">
+        <a
+          href="https://getstuffdone.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <h3 className="project_title">MarketPlace</h3>
+          {/* <p>
+            Pair programmed with classmates - inspired by my classmate who
+            wanted to bring people closer and allow them to help each other
+          </p> */}
+          <img
+            className=" project-img marketplace"
+            src={marketplace}
+            alt="website look"
+          />
+        </a>
+        <a
+          className="project_button"
+          href="https://github.com/melaniebuck10/task-marketplace"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
       </div>
     </section>
   );
