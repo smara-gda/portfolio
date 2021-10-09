3ff3b8972e0db2af0b6f04892b5b7f779fee9f5f
Smaragda Kalogeropoulou <smaragda_kal@hotmail.com>
2021-10-09 22:17:44 +0200

    attempting hamburger menu on mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,45 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import './Header.scss';
 
 const Header = (props) => {
+  const [navBarOpen, setNavBarOpen] = useState(false);
+
+  const handleToggle = () => {
+    setNavBarOpen((prev) => !prev);
+  };
   return (
     <header className="header">
-      <nav className="nav">
-        <Link
-          to="/"
-          onClick={() =>
-            props.navRefs[0].current.scrollIntoView({ behavior: 'smooth' })
-          }
-        >
-          Home{' '}
-        </Link>
-
-        <Link
-          to="/"
-          onClick={() =>
-            props.navRefs[2].current.scrollIntoView({ behavior: 'smooth' })
-          }
-        >
-          Projects
-        </Link>
-        <Link
-          to="/"
-          onClick={() =>
-            props.navRefs[1].current.scrollIntoView({ behavior: 'smooth' })
-          }
-        >
-          Skills
-        </Link>
-        <Link
-          to="/"
-          onClick={() =>
-            props.navRefs[3].current.scrollIntoView({ behavior: 'smooth' })
-          }
-        >
-          About Me
-        </Link>
+      <nav className="navBar">
+        <button onClick={handleToggle}>{navBarOpen ? 'Close' : 'Open'}</button>
+        <ul className={`mobile-view-nav ${navBarOpen ? ' showMenu' : ''}`}>
+          <li>
+            <Link
+              to="/"
+              onClick={() =>
+                props.navRefs[0].current.scrollIntoView({ behavior: 'smooth' })
+              }
+            >
+              Home{' '}
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/"
+              onClick={() =>
+                props.navRefs[2].current.scrollIntoView({ behavior: 'smooth' })
+              }
+            >
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/"
+              onClick={() =>
+                props.navRefs[1].current.scrollIntoView({ behavior: 'smooth' })
+              }
+            >
+              Skills
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/"
+              onClick={() =>
+                props.navRefs[3].current.scrollIntoView({ behavior: 'smooth' })
+              }
+            >
+              About Me
+            </Link>
+          </li>
+        </ul>
       </nav>
     </header>
   );
